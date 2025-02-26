4cb1276a504dbc1666f9ddb1c6968f7c83b140f6
kiplah <kiplahvictor27@gmail.com>
2025-02-26 14:51:12 +0300

    improved

--- includes/footer.php
+++ includes/footer.php
@@ -1,46 +1,53 @@
-<footer class="footer bg-dark text-white py-4">
+<footer class="footer bg-dark text-white py-5">
     <div class="container">
         <div class="row">
-            <!-- Left: Quick Links -->
-            <div class="col-md-6">
-                <h5>Quick Links</h5>
+            <!-- Quick Links -->
+            <div class="col-md-3">
+                <h5 class="footer-title">QUICK LINKS</h5>
                 <ul class="list-unstyled">
-                    <li><a href="index.php" class="text-white text-decoration-none">Home</a></li>
-                    <li><a href="about.php" class="text-white text-decoration-none">About</a></li>
-                    <li><a href="projects.php" class="text-white text-decoration-none">Projects</a></li>
-                    <li><a href="contact.php" class="text-white text-decoration-none">Contact</a></li>
+                    <li><a href="index.php" class="text-white">Home</a></li>
+                    <li><a href="about.php" class="text-white">About</a></li>
+                    <li><a href="services.php" class="text-white">Services</a></li>
+                    <li><a href="blog.php" class="text-white">Blog</a></li>
+                    <li><a href="contact.php" class="text-white">Contact</a></li>
                 </ul>
             </div>
 
-            <!-- Right: Social Media -->
-            <div class="col-md-6 text-md-end">
-                <h5>Follow Me</h5>
-                <div class="social-icons mt-2">
-                    <a href="https://linkedin.com/in/yourprofile" target="_blank" class="text-white me-3">
-                        <i class="fab fa-linkedin fa-lg"></i>
-                    </a>
-                    <a href="https://github.com/yourgithub" target="_blank" class="text-white me-3">
-                        <i class="fab fa-github fa-lg"></i>
-                    </a>
-                    <a href="https://twitter.com/yourtwitter" target="_blank" class="text-white me-3">
-                        <i class="fab fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="https://instagram.com/yourinstagram" target="_blank" class="text-white me-3">
-                        <i class="fab fa-instagram fa-lg"></i>
-                    </a>
-                    <a href="https://facebook.com/yourfacebook" target="_blank" class="text-white me-3">
-                        <i class="fab fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="https://wa.me/yourwhatsappnumber" target="_blank" class="text-white">
-                        <i class="fab fa-whatsapp fa-lg"></i>
-                    </a>
+            <!-- Contact Info -->
+            <div class="col-md-3">
+                <h5 class="footer-title">CONTACT INFO</h5>
+                <p>Phone: <a href="tel:+254703924936" class="text-white">+254 703 924 936</a></p>
+                <p>Email: <a href="mailto:vicktechsolutions4@gmail.com" class="text-white">vicktechsolutions4@gmail.com</a></p>
+                <p>Address: Karatina, Kenya</p>
+            </div>
+
+            <!-- Social Media -->
+            <div class="col-md-3">
+                <h5 class="footer-title">FOLLOW ME</h5>
+                <div class="social-icons">
+                    <a href="https://facebook.com/yourfacebook" target="_blank"><img src="icons/facebook.png" alt="Facebook"></a>
+                    <a href="https://wa.me/254703924936" target="_blank"><img src="icons/whatsapp.png" alt="WhatsApp"></a>
+                    <a href="https://twitter.com/yourtwitter" target="_blank"><img src="icons/twitter.png" alt="Twitter"></a>
+                    <a href="https://instagram.com/yourinstagram" target="_blank"><img src="icons/instagram.png" alt="Instagram"></a>
+                    <a href="https://linkedin.com/in/yourlinkedin" target="_blank"><img src="icons/linkedin.png" alt="LinkedIn"></a>
+                    <a href="https://github.com/yourgithub" target="_blank"><img src="icons/github.png" alt="GitHub"></a>
                 </div>
+            </div>
+
+            <!-- Newsletter -->
+            <div class="col-md-3">
+                <h5 class="footer-title">SUBSCRIBE TO MY NEWSLETTER</h5>
+                <p>Get the latest updates and special offers</p>
+                <form>
+                    <input type="email" class="form-control mb-2" placeholder="Enter your email">
+                    <button class="btn btn-danger w-100">Subscribe</button>
+                </form>
             </div>
         </div>
 
         <!-- Copyright -->
-        <div class="text-center mt-3">
-            <p class="mb-0">&copy; <?php echo date("Y"); ?> Victor Kiplangat. All Rights Reserved.</p>
+        <div class="text-center mt-4">
+            <p>&copy; <?php echo date("Y"); ?> Victor Kiplangat. All Rights Reserved.</p>
         </div>
     </div>
 </footer>
